17541b2037f6369b733cd98c9268dfef6e8630f3
AlphaNow <simon.palade37@gmail.com>
2022-01-29 09:45:12 +0100

    add an auto-import system for the commands in the commands-folder

--- code/chat/command/__init__.py
+++ code/chat/command/__init__.py
@@ -1,2 +1,8 @@
+import pathlib
+
+
 from .executor import execute
-from .commands import time, weather, block, gamemode
+
+command_path = pathlib.Path(__file__).parent / "commands"
+files = [path.name for path in command_path.iterdir() if not path.name == "__pycache__"]
+__import__("commands", level=1, fromlist=files, globals=globals())
