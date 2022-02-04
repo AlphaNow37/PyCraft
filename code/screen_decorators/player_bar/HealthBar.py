242554eb6a50c7e4e560a1dd82ec3135b8323d88
AlphaNow <simon.palade37@gmail.com>
2022-02-04 18:27:06 +0100

    update the life bar

--- code/screen_decorators/player_bar/HealthBar.py
+++ code/screen_decorators/player_bar/HealthBar.py
@@ -8,8 +8,9 @@ from . import __init__
 class HealthBarManager:
     def __init__(self, game: Game, manager):
         self.game = game
+        empty_surface = icons.load_basic_icon(0, 0)
         self.hearth_surfaces = {
-            name: icons.load_basic_icon(x, y)
+            name: [src := empty_surface.copy(), src.blit(icons.load_basic_icon(x, y), (0, 0))][0]
             for (name, x, y) in [("plain", 4, 0), ("partial", 5, 0), ("empty", 0, 0)]
         }
         self.super_manager: __init__.PlayerBarManager = manager
@@ -24,14 +25,15 @@ class HealthBarManager:
             name: pygame.transform.scale(surface, (height_hearth, height_hearth))
             for (name, surface) in self.hearth_surfaces.items()
         }
-        for x in range(max(nb_heath+nb_partial_heath, 10)):
-            screen_x = x*height_hearth
-            if x < nb_heath:
+        for i in range(max(nb_heath+nb_partial_heath, 10)):
+            y, x = divmod(i, 10)
+            screen_x = x * height_hearth
+            screen_y = y * height_hearth
+            if i < nb_heath:
                 name = "plain"
-            elif x == nb_heath and nb_partial_heath == 1:
+            elif i == nb_heath and nb_partial_heath == 1:
                 name = "partial"
             else:
                 name = None
-            self.game.screen.blit(hearths_surfaces["empty"], (screen_x, 0))
             if name is not None:
-                self.game.screen.blit(hearths_surfaces[name], (screen_x, 0))
+                self.game.screen.blit(hearths_surfaces[name], (screen_x, screen_y))
