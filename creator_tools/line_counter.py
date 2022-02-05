43a047502cd181921340c78bc57916aa12f4ad85
AlphaNow <simon.palade37@gmail.com>
2022-02-05 18:21:10 +0100

    update line_counter.py

--- creator_tools/line_counter.py
+++ creator_tools/line_counter.py
@@ -1,22 +1,27 @@
 import pathlib
+from collections import defaultdict
 
 
 root = pathlib.Path(__file__).parent.parent / "code_src"
 
 
+def fusion_dct(dct1, dct2):
+    return {
+        key: dct1.get(key, 0) + dct2.get(key, 0)
+        for key in (set(dct1) | set(dct2))
+    }
+
+
 def line_counter(path: pathlib.Path):
-    line_count = 0
-    file_count = 0
+    dct = defaultdict(lambda: 0)
     for sub_path in path.iterdir():
         if sub_path.is_file():
             if sub_path.suffix == ".py":
-                line_count += sub_path.read_text("UTF-8").count("\n")
-                file_count += 1
+                dct["lines"] += sub_path.read_text("UTF-8").count("\n")
+                dct["files"] += 1
         else:
-            nb_lines, nb_files = line_counter(sub_path)
-            line_count += nb_lines
-            file_count += nb_files
-    return line_count, file_count
+            dct2 = line_counter(sub_path)
+            dct = fusion_dct(dct, dct2)
+    return dct
 
 print(line_counter(root))
-print(dict.__len__({"1": 2}))
