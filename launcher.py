f97b21e436309609164b9e18728ab9c2cd580826
AlphaNow <simon.palade37@gmail.com>
2022-02-11 19:10:40 +0100

    adding an auto pip-install when a module is not present

--- launcher.py
+++ launcher.py
@@ -1,17 +1,18 @@
 import subprocess
+from sys import version_info
+
 
 for name, pipname in [("pygame",)*2, ("requests",)*2, ("yaml", "PyYaml")]:
     try:
         __import__(name)
     except ImportError:
-        subprocess.run(f"py -m pip install {pipname}")
+        subprocess.run(f"py -3.{version_info.minor} -m pip install {pipname}")
 
 import pygame
 pygame.init()
 
 import code_src
 code_src.Game()
-
 """
 PyCraft
 Par AlphaNow
