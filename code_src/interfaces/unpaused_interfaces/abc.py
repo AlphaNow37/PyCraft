4301c8b92964cdfe94b487ec69b728f6c513378b
AlphaNow <simon.palade37@gmail.com>
2022-03-15 18:53:50 +0100

    added a system to get the index of  case in a grid-interface

--- code_src/interfaces/unpaused_interfaces/abc.py
+++ code_src/interfaces/unpaused_interfaces/abc.py
@@ -10,6 +10,8 @@ class BaseUnpausedInterface(AbcInterface):
     surface: pygame.Surface
 
     grids: list[GridContainer] = []
+    last_rect: pygame.Rect
+    last_surfacesize: tuple[int, int]
 
     def __init__(self, game):
         self.grids = []
@@ -21,12 +23,13 @@ class BaseUnpausedInterface(AbcInterface):
     def draw(self):
         screen_w, screen_h = self.game.size_screen
         src_w, src_h = self.surface.get_size()
+        self.last_surfacesize = src_w, src_h
         my_width, my_height = get_size(screen_w, screen_h, src_w/src_h*1, 0.8, 0.8)
         my_x = int((screen_w-my_width) / 2)
         my_y = int((screen_h-my_height) / 2)
         my_surface = pygame.transform.scale(self.get_surface(), (my_width, my_height))
-
-        self.game.screen.blit(my_surface, [my_x, my_y, my_width, my_height])
+        self.last_rect = pygame.Rect(my_x, my_y, my_width, my_height)
+        self.game.screen.blit(my_surface, self.last_rect)
 
     def get_surface(self) -> pygame.Surface:
         surface = self.surface.copy()
@@ -37,6 +40,17 @@ class BaseUnpausedInterface(AbcInterface):
     def on_event(self, event):
         if event.type == pygame.KEYDOWN and event.key == pygame.K_e:
             self.close()
+        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == pygame.BUTTON_LEFT:
+            x, y = event.pos
+            x -= self.last_rect.x
+            y -= self.last_rect.y
+            x /= self.last_rect.width
+            y /= self.last_rect.height
+            x *= self.last_surfacesize[0]
+            y *= self.last_surfacesize[1]
+            for grid in self.grids:
+                if (pos := grid.get_xy_from_rawpos(x, y)) is not None:
+                    print(pos)
 
     def close(self):
         print("interface closed")
