cc38b6518f4f2510ac1d04cbeee2f3e3a4ba5c48
AlphaNow <simon.palade37@gmail.com>
2022-04-04 21:49:44 +0200

    GameMode is now an IntEnum

--- code_src/constants.py
+++ code_src/constants.py
@@ -6,9 +6,6 @@ from enum import IntEnum
 HEIGHT_WORLD = 100
 HEIGHT_OCEAN = 50
 
-
-# GAMEMODES = ["SURVIVAL", "CREATIVE", "SPECTATOR"]
-
 TIME_INCREMENT = 0.2
 
 PLAYER_RANGE = 5
@@ -21,7 +18,3 @@ class GameMode(IntEnum):
     SURVIVAL = 0
     CREATIVE = 1
     SPECTATOR = 2
-
-
-if __name__ == '__main__':
-    print(set(GameMode) - {GameMode.CREATIVE})
